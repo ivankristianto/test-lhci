b32de167e35fe6021ebaeaa93e1b6385f83e9be3
Ivan Kristianto <ivan.kristianto@gmail.com>
2020-05-22 12:36:47 +0700

    Remove token

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,8 +2,9 @@ module.exports = {
   ci: {
     upload: {
       target: 'lhci',
-      serverBaseUrl: 'https://dry-beyond-35237.herokuapp.com/',
-      token: '36784a32-b008-4201-97bb-3a7ca68427f4', // could also use LHCI_TOKEN variable instead
+    },
+    assert: {
+      preset: 'lighthouse:recommended',
     },
   },
 };
