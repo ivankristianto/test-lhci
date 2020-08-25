470967296eea0bc5e5bd65a2d0eb0ee7850919ab
Ivan Kristianto <ivan.kristianto@gmail.com>
2020-08-25 15:00:00 +0700

    Add fcp

--- lighthouserc.js
+++ lighthouserc.js
@@ -5,8 +5,11 @@ module.exports = {
     },
     assert: {
       preset: 'lighthouse:recommended',
+      // "budgetsFile": "./budget.json",
       "assertions": {
         "dom-size": ["error", {"maxNumericValue": 3000}],
+        "first-contentful-paint": ["warn", {"maxNumericValue": 3000}],
+        "viewport": "error",
         "installable-manifest": "off",
         "offline-start-url": "off",
         "service-worker": "off",
